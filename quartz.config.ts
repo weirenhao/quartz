58f2ba5ca33258d21c79bde9366976a6c90594bd
weirenhao <101034609+weirenhao@users.noreply.github.com>
2024-11-29 15:45:09 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "zh",
+    locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
