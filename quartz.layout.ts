333202ef77ff5964cc05c8e5fd8dcf86a910d72e
weirenhao <101034609+weirenhao@users.noreply.github.com>
2024-12-03 14:28:18 +0800

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -49,10 +49,31 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
-  ],
-  pageBody: [    
-    Component.MobileOnly(Component.TableOfContents()),
-  ],
+    Component.DesktopOnly(
+      Component.RecentNotes({
+        title: "最近更新",
+        showTags: false,
+        limit: 4,
+        filter: (f) => {
+          if (f.filePath?.endsWith("index.md")) {
+            return false
+          }
+          return true
+        },
+        sort: (f1, f2) => {
+          if (f1.dates && f2.dates) {
+            if (Math.abs(f2.dates.modified.getDay() - f1.dates.modified.getDay())<=3) {
+              return f2.dates.created.getTime() - f1.dates.created.getTime()
+            }
+            return f2.dates.modified.getTime() - f1.dates.modified.getTime()
+          } else if (f1.dates && !f2.dates) {
+            return -1
+          }
+          return 1
+        }
+      })
+    ),
+  ]
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
