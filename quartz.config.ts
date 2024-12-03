8c51509847099c90d97acf45742b6e75ab8ea18a
weirenhao <101034609+weirenhao@users.noreply.github.com>
2024-12-03 14:41:16 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -58,7 +58,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["frontmatter",'git'],
+        priority: ['git'],
       }),
       Plugin.SyntaxHighlighting({
         theme: {
